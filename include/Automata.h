706b9ef31832e550cc2972d69be7e27ed3f95a22
Sergey Kamskov <sk1999@inbox.ru>
2021-04-04 18:46:46 +0300

    First commit all files implemented

---
+++ include/Automata.h
@@ -0,0 +1,30 @@
+#include <string>
+
+enum States
+{
+    OFF, WAIT, ACCEPT, CHECK, COOK
+};
+
+class Automata
+{
+    private:
+        int cash;
+        std::string* menu;
+        int* prices;
+        States state;
+        void cook();    
+        bool check(int _choice);
+    
+    public:
+        Automata();
+        void on();
+        void off();
+        void coin(int money);
+        std::string etMenu();
+        States getState();
+        void choice(int choice);
+        int cancel();
+        int finish();
+        
+        ~Automata();
+};
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,42 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task, test1)
+{
+    Automata machine = Automata();
+    machine.on();
+    machine.coin(13);
+    machine.choise(2);
+    States result = coffee.getState();
+    EXPECT_EQ(WAIT, result);
+}
+
+TEST(task, test2)
+{
+    Automata machine = Automata();
+    machine.on();
+    machine.choise(1);
+    States result = machine.getState();
+    EXPECT_EQ(WAIT, result);
+}
+
+TEST(task, test3)
+{
+    Automata machine = Automata();
+    machine.on();
+    machine.coin(20);
+    machine.choise(1);
+    int result = machine.finish();
+    EXPECT_EQ(10, result);
+}
+
+TEST(task, test4)
+{
+    Automata machine = Automata();
+    machine.on();
+    machine.coin(20);
+    machine.coin(30);
+    machine.choise(1);
+    int result = machine.finish();
+    EXPECT_EQ(40, result);
+}
