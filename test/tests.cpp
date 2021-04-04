94082ec7f428648b4c499fdc18b478c2bf0612e5
Sergey Kamskov <sk1999@inbox.ru>
2021-04-04 19:01:04 +0300

    Third

--- test/tests.cpp
+++ test/tests.cpp
@@ -15,7 +15,7 @@ TEST(task, test2)
 {
     Automata machine = Automata();
     machine.on();
-    machine.choice(1);
+    machine.choice(2);
     States result = machine.getState();
     EXPECT_EQ(WAIT, result);
 }
@@ -24,19 +24,8 @@ TEST(task, test3)
 {
     Automata machine = Automata();
     machine.on();
-    machine.coin(20);
-    machine.choice(1);
-    int result = machine.finish();
-    EXPECT_EQ(10, result);
-}
-
-TEST(task, test4)
-{
-    Automata machine = Automata();
-    machine.on();
-    machine.coin(20);
     machine.coin(30);
     machine.choice(1);
     int result = machine.finish();
-    EXPECT_EQ(40, result);
+    EXPECT_EQ(20, result);
 }
