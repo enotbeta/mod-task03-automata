0bd96162d417b0e4095494b4d49f3f79a8596154
Sergey Kamskov <sk1999@inbox.ru>
2021-04-04 19:05:44 +0300

    fourth

--- test/tests.cpp
+++ test/tests.cpp
@@ -11,21 +11,3 @@ TEST(task, test1)
     EXPECT_EQ(WAIT, result);
 }
 
-TEST(task, test2)
-{
-    Automata machine = Automata();
-    machine.on();
-    machine.choice(2);
-    States result = machine.getState();
-    EXPECT_EQ(WAIT, result);
-}
-
-TEST(task, test3)
-{
-    Automata machine = Automata();
-    machine.on();
-    machine.coin(30);
-    machine.choice(1);
-    int result = machine.finish();
-    EXPECT_EQ(20, result);
-}
